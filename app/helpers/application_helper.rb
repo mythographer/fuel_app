c080f8a20ae7659268d35096490f7c96776e3aed
Eduard Kasimov <eduard.kasimov@gmail.com>
2016-10-17 19:14:54 +0300

    Add flash_messages partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def bootstrap_gliphicon_sign_for(message_type)
+      "glyphicon-%s-sign" % { success: 'ok',
+                              info: 'info',
+                              warning: 'warning',
+                              danger: 'exclamation' }[message_type.to_sym]
+    end
 end
